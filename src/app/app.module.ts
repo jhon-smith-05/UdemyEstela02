938f39ff7fb12825cde347114eeb2d4b353cbb86
Jhon Bernal <jsmithbo05@gmail.com>
2025-08-25 23:46:26 -0500

    seccion 24 - 123 Componentes listado con snippets de bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ListadoComponent } from './componentes/listado/listado.component';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
